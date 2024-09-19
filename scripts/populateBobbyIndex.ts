c82ed32ca3ec37b40c3f37ec65e9c9257bbe07f2
maxwellpothier <pothiermaxwell@gmail.com>
2024-09-19 08:51:53 -0600

    stubbing out some functions

--- scripts/populateBobbyIndex.ts
+++ scripts/populateBobbyIndex.ts
@@ -6,3 +6,13 @@ loadEnvConfig("");
 const pc = new Pinecone({apiKey: process.env.PINECONE_API_KEY!});
 
 const indexName = "ask-bobby";
+
+const getEmbeddings = async (chunks: string[]) => {};
+
+const upsertToBobby = async (embeddings: number[][]) => {};
+
+(async () => {
+	const embeddings = await getEmbeddings(["Hello, world!"]);
+
+	await upsertToBobby(embeddings);
+})().catch(console.error);

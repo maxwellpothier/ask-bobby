9dc5341ffdbda768c495f894db77a2b3a254038a
maxwellpothier <pothiermaxwell@gmail.com>
2024-09-19 08:49:12 -0600

    function to chunk the transcript

---
+++ scripts/chunkTranscript.ts
@@ -0,0 +1,30 @@
+import fs from "fs";
+import path from "path";
+import {RecursiveCharacterTextSplitter} from "langchain/text_splitter";
+import {ProcessedVideo} from "@/types";
+
+const getData = () => {
+	const dataPath = path.join(process.cwd(), "data", "processedVideos.json");
+
+	// Read the JSON file
+	const rawData = fs.readFileSync(dataPath, "utf-8");
+	return JSON.parse(rawData);
+};
+
+export const chunkTranscript = async (
+	transcript: string
+): Promise<string[]> => {
+	const textSplitter = new RecursiveCharacterTextSplitter({
+		chunkSize: 1000,
+		chunkOverlap: 200,
+	});
+
+	const chunks = await textSplitter.splitText(transcript);
+	return chunks;
+};
+
+(async () => {
+	const videos: ProcessedVideo[] = getData();
+
+	console.log(await chunkTranscript(videos[100].transcript));
+})().catch(console.error);
--- scripts/populateBobbyIndex.ts
+++ scripts/populateBobbyIndex.ts
@@ -1,2 +1,8 @@
+import {Pinecone} from "@pinecone-database/pinecone";
+import {loadEnvConfig} from "@next/env";
+
+loadEnvConfig("");
+
+const pc = new Pinecone({apiKey: process.env.PINECONE_API_KEY!});
+
 const indexName = "ask-bobby";
-console.log(indexName);
